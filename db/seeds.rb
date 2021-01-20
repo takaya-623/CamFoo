b5a7ba14bbf6bd1e3c8b5cd8abcfb16bffa380e6
takaya-623 <takaya.suzuki.0623@gmail.com>
2021-01-20 21:55:40 +0900

    add login logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  last_name: "山田",
+  first_name: "太郎",
+  last_name_kana: "ヤマダ",
+  first_name_kana: "タロウ",
+  email: "yamada@yamada.com",
+  password: "123456",
+  acount: "@やまだくん"
+  )
