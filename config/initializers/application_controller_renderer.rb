cc9e4e33bb6673b0a3a5a8e29037ab0a5f1e8a67
root <root@3d5905e923c9>
2021-01-21 05:19:13 +0000

    add user_show & edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '703794ca4a8d6ceff93ea12ee6d3dec25ba020d0ae591993c51875c3ce12c7ea694258f5794cef37aa0066fa3eda66e429433c115a04077fa1ecc8abace75502'
