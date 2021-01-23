f0ce9d4d6c25dd1e7e647620290bd323992e7ee4
takaya-623 <takaya.suzuki.0623@gmail.com>
2021-01-23 16:55:41 +0900

    add likes_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  # before_action :confirm_user, only: [:edit, :update]
+  before_action :confirm_user, only: [:edit, :update]
 
-  # def confirm_user
-  #   user = User.find(params[:id])
-  #   unless user == current_user
-  #     redirect_to user_path(current_user)
-  #   end
-  # end
+  def confirm_user
+    user = User.find(params[:id])
+    unless user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 
   def show
     @user = User.find(params[:id])
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:acount, :account_image)
+    params.require(:user).permit(:account, :account_image)
   end
 end
 
