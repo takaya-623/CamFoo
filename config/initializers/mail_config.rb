7a83c6410f7b24cfc82b46aaaaf73eca7d329240
takaya-623 <takaya.suzuki.0623@gmail.com>
2021-02-12 23:26:23 +0900

    edit devese

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_url_options = { :host => 'localhost:3000'}
+ActionMailer::Base.default_url_options = { :host => 'http://camfoo-portfolio.work/'}
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
